34a2488802ecdb77198758d7d4ed0f9578a261d2
Oussama Jaaouani <jaaouani@spottd-app.com>
2020-04-08 14:57:13 +0200

    fixedd

--- configuration.js
+++ configuration.js
@@ -1,5 +1,5 @@
 export const configuration = {
         MAP_ACCESS_TOKEN   :    'pk.eyJ1IjoiamFhb3VhbmlvIiwiYSI6ImNrNWEwOGZ4azEyZmkzdXBhMHgwMGhldTAifQ.aB0IgnpP9Z3IPZiVZLZSkQ',
-        MAP_STYLE_URL      :    'mapbox://styles/jaaouanio/ck7xh7s0y1a2d1irqfj8a3rc6',
+        MAP_STYLE_URL      :    'mapbox://styles/jaaouanio/ck8phzyra0u0d1iqjw94hgvan',
         APOLLO_SERVER_URI  :    'http://localhost:4000/graphql'
 };
